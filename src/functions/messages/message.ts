33c3929c0d7dba842fffbf30221963f576b017ac
CharlesPeterMcCarthy <charles@studybundles.com>
2019-09-01 22:07:12 +0100

    Removed Demo Messages Websocket

--- src/functions/messages/message.ts
+++
@@ -1,6 +0,0 @@
-import { ApiHandler } from '../../responses/api.types';
-import { MessageController } from './message.controller';
-
-const controller: MessageController = new MessageController();
-
-export const sendMessageHandler: ApiHandler = controller.sendMessage;
