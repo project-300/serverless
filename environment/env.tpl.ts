4198b028f62b6d504e6c29f39044cfb03668b51d
CharlesPeterMcCarthy <charles@studybundles.com>
2019-09-03 16:51:51 +0100

    Environment Template & File

---
+++ environment/env.tpl.ts
@@ -0,0 +1,1 @@
+export const WEBSOCKET_ENDPOINT: string = '{{ WEBSOCKET_ENDPOINT }}';
---
+++ environment/env.ts
@@ -0,0 +1,1 @@
+export const WEBSOCKET_ENDPOINT: string = '29djb951zd.execute-api.eu-west-1.amazonaws.com/dev';
