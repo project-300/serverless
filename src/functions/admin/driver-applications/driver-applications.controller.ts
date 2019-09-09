44c23f20183792663e0fc1281d3588e74443c62f
CharlesPeterMcCarthy <charles@studybundles.com>
2019-09-09 20:15:18 +0100

    Admin Driver Applications Functions

---
+++ src/functions/admin/driver-applications/driver-applications.controller.ts
@@ -0,0 +1,152 @@
+import * as AWS from 'aws-sdk';
+import { DocumentClient } from 'aws-sdk/lib/dynamodb/document_client';
+import PubManager from '../../../pubsub/publication';
+import SubManager from '../../../pubsub/subscription';
+import { DeleteResult, GetResult, GetResultPromise, ScanResult, ScanResultPromise, UpdateResult } from '../../../responses/dynamodb.types';
+import { DRIVER_APPLICATION_INDEX, USERS_INDEX } from '../../../constants/indexes';
+import { DRIVER_APPLICATION_TABLE, USER_TABLE } from '../../../constants/tables';
+import { ResponseBuilder } from '../../../responses/response-builder';
+import {
+	DriverApplicationApprovalResult,
+	DriverApplicationDeleteResult,
+	DriverApplicationsSubscriptionResult
+} from './driver-applications.interfaces';
+import { ApiCallback, ApiContext, ApiEvent, ApiHandler } from '../../../responses/api.types';
+import UpdateItemInput = DocumentClient.UpdateItemInput;
+import ScanInput = DocumentClient.ScanInput;
+import DeleteItemInput = DocumentClient.DeleteItemInput;
+import GetItemInput = DocumentClient.GetItemInput;
+
+export class DriverApplicationController {
+
+	private dynamo: DocumentClient = new AWS.DynamoDB.DocumentClient();
+
+	public getApplications: ApiHandler = async (event: ApiEvent, context: ApiContext, callback: ApiCallback): Promise<void> => {
+		const result: DriverApplicationsSubscriptionResult = {
+			success: true
+		};
+
+		const body = JSON.parse(event.body);
+
+		try {
+			if (body.subscribe) {
+				const data: ScanResult = await this._applications();
+				await SubManager.subscribe(
+					event,
+					body.subscription,
+					DRIVER_APPLICATION_INDEX,
+					data.Items,
+					true
+				);
+			} else {
+				await SubManager.unsubscribe(body.subscription, event.requestContext.connectionId);
+			}
+
+			ResponseBuilder.ok<DriverApplicationsSubscriptionResult>(result, callback);
+		} catch (err) {
+			ResponseBuilder.internalServerError(err, callback);
+		}
+	}
+
+	private _applications = (): ScanResultPromise => {
+		const params: ScanInput = {
+			TableName: DRIVER_APPLICATION_TABLE
+		};
+
+		return this.dynamo.scan(params).promise();
+	}
+
+	public approveApplication: ApiHandler = async (event: ApiEvent, context: ApiContext, callback: ApiCallback): Promise<void> => {
+		const result: DriverApplicationApprovalResult = {
+			success: true
+		};
+
+		const userId: string = JSON.parse(event.body).userId;
+
+		try {
+			await this._updateUserType(userId);
+			await this._updateApplication(userId);
+			const updatedApplication: GetResult = await this._retrieveApplication(userId);
+			await PubManager.publishUpdate('admin/driver-applications', DRIVER_APPLICATION_INDEX, updatedApplication.Item);
+
+			ResponseBuilder.ok<DriverApplicationApprovalResult>(result, callback);
+		} catch (err) {
+			ResponseBuilder.internalServerError(err, callback, err.message);
+		}
+	}
+
+	private _retrieveApplication = (userId: string): GetResultPromise => {
+		const params: GetItemInput = {
+			TableName: DRIVER_APPLICATION_TABLE,
+			Key: {
+				[DRIVER_APPLICATION_INDEX]: userId
+			}
+		};
+
+		return this.dynamo.get(params).promise();
+	}
+
+	private _updateUserType = (userId: string): UpdateResult => {
+
+		const params: UpdateItemInput = {
+			TableName: USER_TABLE,
+			Key: {
+				[USERS_INDEX]: userId
+			},
+			UpdateExpression: 'SET userType = :type',
+			ExpressionAttributeValues: {
+				':type': 'Driver'
+			},
+			ReturnValues: 'UPDATED_NEW'
+		};
+
+		return this.dynamo.update(params).promise();
+	}
+
+	private _updateApplication = (userId: string): UpdateResult => {
+
+		const params: UpdateItemInput = {
+			TableName: DRIVER_APPLICATION_TABLE,
+			Key: {
+				[DRIVER_APPLICATION_INDEX]: userId
+			},
+			UpdateExpression: 'SET approved = :app, times.approved = :dt',
+			ExpressionAttributeValues: {
+				':app': true,
+				':dt': new Date().toISOString()
+			},
+			ReturnValues: 'UPDATED_NEW'
+		};
+
+		return this.dynamo.update(params).promise();
+	}
+
+	public deleteApplication: ApiHandler = async (event: ApiEvent, context: ApiContext, callback: ApiCallback): Promise<void> => {
+		const result: DriverApplicationDeleteResult = {
+			success: true
+		};
+
+		const userId: string = JSON.parse(event.body).userId;
+
+		try {
+			await this._deleteApplication(userId);
+			await PubManager.publishDelete('admin/driver-applications', DRIVER_APPLICATION_INDEX, userId);
+			ResponseBuilder.ok<DriverApplicationApprovalResult>(result, callback);
+		} catch (err) {
+			ResponseBuilder.internalServerError(err, callback, err.message);
+		}
+	}
+
+	private _deleteApplication = (userId: string): DeleteResult => {
+
+		const params: DeleteItemInput = {
+			TableName: DRIVER_APPLICATION_TABLE,
+			Key: {
+				[DRIVER_APPLICATION_INDEX]: userId
+			}
+		};
+
+		return this.dynamo.delete(params).promise();
+	}
+
+}
---
+++ src/functions/admin/driver-applications/driver-applications.interfaces.ts
@@ -0,0 +1,11 @@
+export interface DriverApplicationApprovalResult {
+	success: boolean;
+}
+
+export interface DriverApplicationDeleteResult {
+	success: boolean;
+}
+
+export interface DriverApplicationsSubscriptionResult {
+	success: boolean;
+}
