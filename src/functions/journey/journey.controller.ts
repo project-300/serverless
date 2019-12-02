d9c1e8563f54ddc68e6f874ae16535ea4f6974d6
Brian Keaveney <briankeaveney@hotmail.com>
2019-12-02 02:57:26 +0000

    update when user joins a journey

--- src/functions/journey/journey.controller.ts
+++ src/functions/journey/journey.controller.ts
@@ -1,13 +1,68 @@
 import * as AWS from 'aws-sdk';
 import { DocumentClient } from 'aws-sdk/lib/dynamodb/document_client';
 import { ResponseBuilder } from '../../responses/response-builder';
-import { GetResultPromise, ScanResult } from '../../responses/dynamodb.types';
+import {
+  GetResultPromise,
+  ScanResult,
+  UpdateResult
+} from '../../responses/dynamodb.types';
 import { ApiEvent, ApiHandler, ApiResponse } from '../../responses/api.types';
 import ScanInput = DocumentClient.ScanInput;
+import UpdateItemInput = DocumentClient.UpdateItemInput;
 import { JOURNEY_TABLE } from '../../constants/tables';
+import { JOURNEY_INDEX } from '../../constants/indexes';
 
 export class JourneyController {
   private dynamo: DocumentClient = new AWS.DynamoDB.DocumentClient();
+
+  public userJoinedJourney: ApiHandler = async (
+    event: ApiEvent
+  ): Promise<ApiResponse> => {
+    const data = JSON.parse(event.body);
+
+    if (data.seatsLeft <= 0) {
+      return ResponseBuilder.internalServerError(
+        Error('No Seats Available'),
+        'Unable to update journey'
+      );
+    }
+
+    if (data.journey.driver.userId === data.userId) {
+      return ResponseBuilder.internalServerError(
+        Error('Driver cannot join own journey'),
+        'Unable to update journey'
+      );
+    }
+
+    try {
+      await this._updateJourney(data.journey.journeyId, data.userId);
+
+      return ResponseBuilder.ok({ success: true });
+    } catch (err) {
+      return ResponseBuilder.internalServerError(
+        err,
+        'Unable to update journey'
+      );
+    }
+  };
+
+  private _updateJourney = (journeyId, userId): UpdateResult => {
+    const params: UpdateItemInput = {
+      TableName: JOURNEY_TABLE,
+      Key: {
+        [JOURNEY_INDEX]: journeyId
+      },
+      UpdateExpression:
+        'set seatsLeft - :seat, passengers = list_append(passengers, :newPassenger)',
+      ExpressionAttributeValues: {
+        ':seat': 1,
+        ':newPassenger': userId
+      },
+      ReturnValues: 'UPDATED_NEW'
+    };
+
+    return this.dynamo.update(params).promise();
+  };
 
   public allJourneys: ApiHandler = async (
     event: ApiEvent
@@ -24,9 +79,10 @@ export class JourneyController {
   private _getAllJourneys = (): GetResultPromise => {
     const params: ScanInput = {
       TableName: JOURNEY_TABLE,
-      FilterExpression: 'journeyStatus = :status',
+      FilterExpression: 'journeyStatus = :status and seatsLeft > :value',
       ExpressionAttributeValues: {
-        ':status': 'NOT_STARTED'
+        ':status': 'NOT_STARTED',
+        ':value': 0
       }
     };
 
