9c4d43018ac83f4547aa727339439624d0444c9e
Charles McCarthy <charles@studybundles.com>
2020-02-12 19:03:06 +0000

    Offline User ID Bug Fix

--- services/api-shared-modules/src/utils/shared-functions.ts
+++ services/api-shared-modules/src/utils/shared-functions.ts
@@ -5,7 +5,7 @@ export class SharedFunctions {
 	public static getUserIdFromAuthProvider = (authProvider: string): string => {
 		const parts: string[] = authProvider.split(':');
 
-		const userId: string = process.env.IS_OFFLINE ? parts[parts.length - 1] : USERID_FOR_TESTING;
+		const userId: string = process.env.IS_OFFLINE ? USERID_FOR_TESTING : parts[parts.length - 1];
 
 		return userId;
 	}
