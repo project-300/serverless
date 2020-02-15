a9f49fc6350f919a35297d0ce1217b8e63ef9f07
Brian Keaveney <38507940+BrianKeaveney@users.noreply.github.com>
2020-02-15 16:52:50 +0000

    statistics boilerplate added

---
+++ services/api-shared-modules/src/models/core/Statistics.ts
@@ -0,0 +1,4 @@
+import { DynamoDbItem } from './../DynamoDBItem';
+import { attribute } from '@aws/dynamodb-data-mapper-annotations';
+
+// export class StatisticsItem extends DynamoDbItem implements Statistic
