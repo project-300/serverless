7e34ba1b5ecea937d0ce62716209d517aef674d9
Charles McCarthy <charles@studybundles.com>
2020-03-07 19:54:44 +0000

    Rename Service

--- services/api-interest/src/interest.controller.ts
+++ services/api-interest/src/interest.controller.ts
@@ -20,9 +20,6 @@ export class InterestController {
 			const user: User = await this.unitOfWork.Users.getById(userId);
 			SharedFunctions.checkUserRole([ 'Moderator' ], user.userType);
 
-			delete user.connections;
-			console.log(user);
-
 			if (!user.university) throw Error('You are not associated with any university');
 
 			const university: University = await this.unitOfWork.Universities.getById(user.university.universityId, user.university.name);
